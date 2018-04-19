3277c1b68e928782958aa6b62780c356cad1ab1f
hieu193 <38126493+hieu193@users.noreply.github.com>
2018-04-19 09:02:08 +0700

    Update bai4.php

--- bai4.php
+++ bai4.php
@@ -1,3 +1,9 @@
 <?php
-  $thongtinsanpham = array('tên sản phẩm', 'tên nhà sản xuất', 'ngày sản xuất','thông tin sản phẩm', 'giá bán',);
+  $thongtinsanpham = array(
+    $ten = array('tên sản phẩm');
+    $nhasx = array('tên nhà sản xuất')
+    $ngaysx = array('ngày sản xuất');
+    $thongtinsp = array('thông tin sản phẩm')
+    $gia = array('giá bán',);
+   );
   ?>
