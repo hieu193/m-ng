0b705434994df03b5dfa9c446b9ded8f4d101359
hieu193 <38126493+hieu193@users.noreply.github.com>
2018-04-19 10:19:32 +0700

    Update bai2.php

--- bai2.php
+++ bai2.php
@@ -1,6 +1,16 @@
 <?php 
 $DSgiaovien_hocvien = array(
- 'giao_vien' => array('ten', 'ngay sinh', 'mon day'),
- 'hoc vien' => array('MSSV', 'ten', 'ngay sinh', 'gioi tinh', 'nganh hoc'),
+ 'giao_vien' => array(
+         'ten' => '..........' ,
+         'ngay sinh' => '.........' ,
+         'mon day' => '...........' ,
+       ),
+ 'hoc vien' => array( 
+         'MSSV' => '......' , 
+         'ten' => '..........' , 
+         'ngay sinh' => '.........' ,
+         'gioi tinh' => '.........' ,
+         'nganh hoc' => '.........' ,
+       ),
  );
 ?>
