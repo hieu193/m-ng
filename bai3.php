36a796be0326287ec3334e2cd254136b3384f976
hieu193 <38126493+hieu193@users.noreply.github.com>
2018-04-19 10:10:24 +0700

    Update bai3.php

--- bai3.php
+++ bai3.php
@@ -1,7 +1,7 @@
 <?php
   $thongtinbaiviet = array(
-    $ngay_dang = array('ngày đăng');
-    $gio_dang = array('giờ đăng');
-    $noi_dung = array('nội dung bài viết');
+    'thoigian' => array('ngay', 'gio'),
+    'tieude' = '............',
+    'noidung' = '...........',
    ); 
   ?>
